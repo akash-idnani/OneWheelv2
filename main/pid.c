ea9633ff962bb5a0819dc6a14d5b0db3ce0d9242
Akash Idnani <aidnanidev@gmail.com>
2019-04-20 00:25:04 -0700

    pid constant loop time

--- main/pid.c
+++ main/pid.c
@@ -6,19 +6,16 @@ float setpoint = 0;
 float err_sum, last_err;
 float kP, kI, kD;
 
-float pid_compute(float input) {
-    unsigned long now;
-    get_ms(&now);
-    float time_change = (float) now - last_time;
+int sample_time = 5;
 
+float pid_compute(float input) {
     float error = setpoint - input;
-    err_sum += (error * time_change);
-    double deriv_err = (error - last_err) / time_change;
+    err_sum += error;
+    double deriv_err = error - last_err;
 
     float output = kP * error + kI * err_sum + kD * deriv_err;
 
     last_err = error;
-    last_time = now;
 
     if (output > 255) return 255;
     if (output < -255) return -255;
@@ -26,7 +23,16 @@ float pid_compute(float input) {
 }
 
 void pid_set_tunings(float new_kp, float new_ki, float new_kd) {
+    float sample_time_in_sec = ((float) sample_time) / 1000;
+
     kP = new_kp;
-    kI = new_ki;
-    kD = new_kd;
+    kI = new_ki * sample_time_in_sec;
+    kD = new_kd / sample_time_in_sec;
+}
+
+void set_sample_time(int new_sample_time) {
+    float ratio  = (float) new_sample_time / (float) sample_time;
+    kI *= ratio;
+    kD /= ratio;
+    sample_time = (unsigned long) new_sample_time;
 }
